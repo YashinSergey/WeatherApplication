b595edb6f1aa980b0c9521ed583fda2218fc4637
mr.Blue <yashinsergey125@gmail.com>
2019-08-14 17:25:58 +0300

    Added color change for text depending on the theme

--- app/src/main/java/com/example/weatherapplication/fragments/AboutDeveloperFragment.java
+++ app/src/main/java/com/example/weatherapplication/fragments/AboutDeveloperFragment.java
@@ -1,28 +1,39 @@
 package com.example.weatherapplication.fragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.weatherapplication.R;
+import com.example.weatherapplication.WeatherActivity;
 
 public class AboutDeveloperFragment extends Fragment {
+
+    WeatherActivity activity;
+    TextView textView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity = (WeatherActivity) getActivity();
     }
 
+    @SuppressLint("ResourceAsColor")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about_developer, container, false);
+        View view = inflater.inflate(R.layout.fragment_about_developer, container, false);
+        textView = view.findViewById(R.id.about_developer_textView);
+        if (activity.isDarkTheme()) {
+            textView.setTextColor(Color.WHITE);
+        }
+        return view;
     }
 }
