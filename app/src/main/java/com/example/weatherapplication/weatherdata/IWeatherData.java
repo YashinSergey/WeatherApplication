c42a8336ef13755765a6c5d7470da16952bbf284
mr.Blue <yashinsergey125@gmail.com>
2019-08-17 12:51:11 +0300

    fixed compile error

--- app/src/main/java/com/example/weatherapplication/weatherdata/IWeatherData.java
+++ app/src/main/java/com/example/weatherapplication/weatherdata/IWeatherData.java
@@ -1,5 +1,7 @@
 package com.example.weatherapplication.weatherdata;
 
+import com.example.weatherapplication.weatherdata.models.WeatherRequestModel;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -7,7 +9,7 @@ import retrofit2.http.Query;
 public interface IWeatherData {
 
     @GET("data/2.5/weather")
-    Call<WeatherData> loadWeather(@Query("q") String city,
-                                  @Query("appid") String keyApi,
-                                  @Query("units")String units);
+    Call<WeatherRequestModel> loadWeather(@Query("q") String city,
+                                          @Query("appid") String keyApi,
+                                          @Query("units")String units);
 }
